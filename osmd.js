c87127db2257d42fb0c856400b74ded8a0031687
Darius Foo <darius.foo.tw@gmail.com>
2023-10-02 22:40:47 +0800

    Fix rests counting as notes in rendered score

--- osmd.js
+++ osmd.js
@@ -46,6 +46,10 @@ let OSMD = (function () {
 
   function next() {
     osmd.cursor.next();
+    // skip rests. this hides the rest of the score until it's time for the note, though. unfortunately we don't have anything to use to tell when rests occur, as we don't play rests or spawn pipes for them, so this is the best we can do for now
+    while (osmd.cursor.NotesUnderCursor()[0].pitch === undefined) {
+      osmd.cursor.next();
+    }
     scroll();
   }
 
